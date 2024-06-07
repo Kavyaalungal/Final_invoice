33216193ee763e112732d8dceef91a7fbb0d6d88
Kavyaalungal <142202840+Kavyaalungal@users.noreply.github.com>
2024-06-07 12:53:35 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,43 @@
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import Invoice from './Invoice';
+import Navbar from './Navbar';
+import InvoiceData from './InvoiceData';
+import SingleFieldFetch from './SingleFieldFetch';
+import Swr from './Fetch';
+import Fetch from './Fetch';
+import Single from './Single';
+import Search from './Search';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#4caf50', // Custom primary color
+    },
+    secondary: {
+      main: '#ff9800', // Custom secondary color
+    },
+    default: {
+      main: '#9e9e9e', // Custom default color
+    },
+  },
+});
+
+function App() {
+  return (
+    <div className='app-container'>
+      {/* <Navbar/> */}
+
+    
+    <ThemeProvider theme={theme}>
+      {/* <Search/> */}
+      {/* <SingleFieldFetch/> */}
+      <Single/>
+      {/* <InvoiceData/> */}
+      {/* <Invoice /> */}
+      {/* <Fetch/> */}
+    </ThemeProvider>
+    </div>
+  );
+}
+
+export default App;
